bb2167d7802445be7208b41866e26ce93ba2d210
LarsZauberer <wasser.ian@gmail.com>
2020-10-22 16:18:20 +0200

    File structure

---
+++ library/message.py
@@ -0,0 +1,14 @@
+import logging
+
+
+class Message:
+    def __init__(self, msg: str):
+        pass
+
+
+    def encode(self, key: int) -> str:
+        pass
+
+
+    def decode(self, key: int) -> str:
+        pass
