76112910b11b8d9cae112712d48e0b7eedee4195
LarsZauberer <wasser.ian@gmail.com>
2020-10-29 16:25:52 +0100

    Main sending test

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import logging
 import argparse
+import library.networking as net
 
 
+ip = input(">")
 
+con = net.Connection(ip)
+
+cmd = input(">")
+
+if cmd == "server":
+    con.receive()
+else:
+    con.send([65])
